c961d20a4c8e00db25118fad243d07b0b3e17f3f
Grahp <grahp@grahp.org>
2024-10-24 16:50:25 -0400

    Add log messages while building

--- src/ametrine/core.clj
+++ src/ametrine/core.clj
@@ -5,22 +5,26 @@
 
 (defn -main
   [& args]
+  (println "Started building...")
   (let [config (utils/load-resource "config")
         user-config (utils/load-resource "user")
         output-file (if-let [user-file ((user-config :settings) :output-file)]
                       user-file
                       ((config :settings) :output-file))
+        _ (println "Config loaded.")
         dict (merge (dict/generate
                       (config :starters)
                       (config :vowels)
                       (config :enders))
                     (config :overrides))
+        _ (println "Main dict built.")
         user-dict (merge dict
                          (dict/generate
                            (user-config :starters)
                            (user-config :vowels)
                            (user-config :enders))
                          (user-config :overrides))
+        _ (println "User dict built.")
         output-dict (utils/transform-json user-dict)]
     (utils/spit-resource output-file output-dict)
-    (println "Finished.")))
+    (println "Finished!")))
