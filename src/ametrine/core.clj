d4efcb8c4673811ad7d6f95d5085e0fa6b52b56f
Grahp <grahp@grahp.org>
2024-10-24 19:42:55 -0400

    Make core.clj very beautiful

--- src/ametrine/core.clj
+++ src/ametrine/core.clj
@@ -10,21 +10,14 @@
         user-config (utils/load-resource "user")
         output-file (if-let [user-file ((user-config :settings) :output-file)]
                       user-file
-                      ((config :settings) :output-file))
-        _ (println "Config loaded.")
-        dict (merge (dict/generate
-                      (config :starters)
-                      (config :vowels)
-                      (config :enders))
-                    (config :overrides))
-        _ (println "Main dict built.")
-        user-dict (merge dict
-                         (dict/generate
-                           (user-config :starters)
-                           (user-config :vowels)
-                           (user-config :enders))
-                         (user-config :overrides))
-        _ (println "User dict built.")
-        output-dict (utils/transform-json user-dict)]
-    (utils/spit-resource output-file output-dict)
+                      ((config :settings) :output-file))]
+
+    (println "Config loaded.")
+
+    (-> (merge (dict/generate (config :starters) (config :vowels) (config :enders))
+               (config :overrides)
+               (dict/generate (user-config :starters) (user-config :vowels) (user-config :enders))
+               (user-config :overrides))
+        utils/transform-json
+        (->> (utils/spit-resource output-file)))
     (println "Finished!")))
