03bc040abe26b48fcfaf0392ba9d29ecd92a09f0
Grahp <grahp@grahp.org>
2024-10-24 12:48:52 -0400

    Add user config file

--- src/ametrine/core.clj
+++ src/ametrine/core.clj
@@ -6,12 +6,21 @@
 (defn -main
   [& args]
   (let [config (utils/load-resource "config")
-        output-file ((config :settings) :output-file)
+        user-config (utils/load-resource "user")
+        output-file (if-let [user-file ((user-config :settings) :output-file)]
+                      user-file
+                      ((config :settings) :output-file))
         dict (merge (dict/generate
                       (config :starters)
                       (config :vowels)
                       (config :enders))
                     (config :overrides))
-        output-dict (utils/transform-json dict)]
+        user-dict (merge dict
+                         (dict/generate
+                           (user-config :starters)
+                           (user-config :vowels)
+                           (user-config :enders))
+                         (user-config :overrides))
+        output-dict (utils/transform-json user-dict)]
     (utils/spit-resource output-file output-dict)
     (println "Finished.")))
