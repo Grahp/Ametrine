7fe41d4c8436d70117e73638aba0f1ff4ff9edfc
Grahp <grahp@grahp.org>
2025-03-27 22:36:19 -0400

    Switch License

--- project.clj
+++ project.clj
@@ -1,7 +1,4 @@
 (defproject ametrine "1"
-  :description "Finger spelling"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/data.json "2.5.0"]]
   :main ^:skip-aot dev.grahp.ametrine.core
